b94daaa13601f336ed22e81047f4725400ddd47a
kokos314 <kokos@wp4b.pl>
2017-07-29 12:38:14 +0200

    2017-07-29 12:38

--- install.php
+++ install.php
@@ -127,6 +127,12 @@
             }
             
             //--------------------------------------------------------
+            if( !file_exists( "./cache" ) ){
+                log_html( "mkdir cache", 's' );
+                mkdir('./cache');
+            }
+            
+            //--------------------------------------------------------
             copy( 'config.example.php', './config/config.php' );
             $str = file_get_contents( './config/config.php' );
             
@@ -145,9 +151,127 @@
             $str_s = "define('D_DB_HOST', '".$_POST['D_DB_HOST']."');";
             $str = preg_replace("/define\(\'D_DB_HOST\'\,\s+\'([^\']*)\'\)\;/i", $str_s, $str);
             
-            
-            echo "<pre style='color:white;'>".htmlspecialchars($str)."</pre>";
+            //echo "<pre style='color:white;'>".htmlspecialchars($str)."</pre>";
             file_put_contents('./config/config.php', $str);
+            
+            
+            //--------------------------------------------------------
+            if( !file_exists( './inc/function/system.functions.php' ) ){
+                log_html( "system.functions.php", 'e' );
+                die();
+            }
+            require_once './inc/function/system.functions.php';
+            log_html( "system.functions.php", 's' );
+            
+            if( !file_exists( './config/config.php' ) ){
+                log_html( "config.php", 'e' );
+                die();
+            }
+            require_once './config/config.php';
+            log_html( "./config/config.php", 's' );
+            
+            //--------------------------------------------------------
+            $sql = "
+                CREATE TABLE languages
+                (
+                  languages_id serial NOT NULL,
+                  name character varying(128) NOT NULL,
+                  short_code character varying(4) NOT NULL,
+                  active boolean NOT NULL DEFAULT true,
+                  CONSTRAINT languages_pkey PRIMARY KEY (languages_id),
+                  CONSTRAINT languages_short_code_key UNIQUE (short_code)
+                )
+                WITH (
+                  OIDS=FALSE
+                );
+            ";
+            pgsql::getInstance()->query($sql);
+            
+            $sql = "INSERT INTO languages VALUES (1, 'Polski', 'PL');";
+            pgsql::getInstance()->query($sql);
+            
+            $sql = "INSERT INTO languages VALUES (2, 'English', 'EN');";
+            pgsql::getInstance()->query($sql);
+            
+            //--------------------------------------------------------
+            
+            $sql = "
+                CREATE TABLE www_bots
+                (
+                  www_bots_id serial NOT NULL,
+                  name character varying,
+                  ip character varying(64),
+                  comment character varying(128),
+                  ip_min character(16),
+                  ip_max character varying(16),
+                  CONSTRAINT www_bots_pkey PRIMARY KEY (www_bots_id)
+                )
+                WITH (
+                  OIDS=FALSE
+                );
+            ";
+            pgsql::getInstance()->query($sql);
+            
+            
+            $sql = "
+                CREATE TABLE statistic
+                (
+                  statistic_id serial NOT NULL,
+                  ip character varying(24) NOT NULL,
+                  user_agent text,
+                  http_referer text,
+                  refid character varying(128),
+                  cnt integer,
+                  country_code character varying(6),
+                  date_insert timestamp without time zone,
+                  date_edit timestamp without time zone,
+                  http_host character varying(256),
+                  module character varying(256),
+                  action character varying(256),
+                  access_url character varying(2048),
+                  www_bots_id integer,
+                  users_id integer,
+                  screen_w integer,
+                  screen_h integer,
+                  viewport_w integer,
+                  viewport_h integer,
+                  session_id character varying(256),
+                  http_accept_language character varying(64),
+                  parent_id bigint,
+                  page_load_time integer, -- Czas ładowania strony w [ms]
+                  info character varying(16), -- Pole na dodatkow info
+                  date_day date NOT NULL DEFAULT now(), -- Dzien przepisany z date_insert
+                  request_method character varying(64),
+                  CONSTRAINT statistic_pkey PRIMARY KEY (statistic_id),
+                  CONSTRAINT statistic_www_bots_id_fkey FOREIGN KEY (www_bots_id)
+                      REFERENCES www_bots (www_bots_id) MATCH SIMPLE
+                      ON UPDATE NO ACTION ON DELETE NO ACTION
+                )
+                WITH (
+                  OIDS=FALSE
+                );
+            ";
+            pgsql::getInstance()->query($sql);
+            
+            
+            $sql = "
+                CREATE TABLE translations
+                (
+                  translations_id serial NOT NULL,
+                  name_key character varying(1024),
+                  name_val character varying(1024),
+                  languages_id integer,
+                  CONSTRAINT translations_pkey PRIMARY KEY (translations_id),
+                  CONSTRAINT translations_languages_id_fkey FOREIGN KEY (languages_id)
+                      REFERENCES languages (languages_id) MATCH SIMPLE
+                      ON UPDATE NO ACTION ON DELETE NO ACTION,
+                  CONSTRAINT translations_name_key_languages_id_key UNIQUE (name_key, languages_id)
+                )
+                WITH (
+                  OIDS=FALSE
+                );
+            ";
+            pgsql::getInstance()->query($sql);
             
         ?>
         </div>
